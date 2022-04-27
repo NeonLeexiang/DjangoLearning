b32cbe301706f29ef712e468dcb267b4fcf5bd00
NeonLeexiang <neonleexiang@outlook.com>
2022-04-27 19:00:30 +0800

    docker引入

---
+++ compose/production/django/start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python manage.py migrate
+python manage.py runserver
+#python manage.py collectstatic --noinput
+#gunicorn blogproject.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --chdir=/app
